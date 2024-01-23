4d5b25262a6d5748edbc4a23a49f53b8150cd648
shubham-sharma12 <128372303+shubham-sharma12@users.noreply.github.com>
2024-01-23 18:50:59 +0530

    Delete inheritance_db directory

--- inheritance_db/college.py
+++
@@ -1,8 +0,0 @@
-from univ import University
-class College(University):
-	def getColgDet(self):
-		self.cname = input("College: ")
-		self.clocation = input("College location: ")
-
-	def dispColgDet(self):
-		print(self.cname, self.clocation)
